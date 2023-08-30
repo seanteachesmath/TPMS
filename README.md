f0ba2d652b4fde74a5f98635bd11f31c4f75a24c
seanteachesmath <slaverty@uco.edu>
2023-08-30 10:43:19 -0500

    Latest build deployed.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Technology for Professional Mathematics and Statistics: Notes for Fall 2023
+
+This is a work-in-progress and may not always work.
---
+++ docs/lunr-pretext-search-index.js
@@ -0,0 +1,40 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon-1",
+  "level": "1",
+  "url": "colophon-1.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "   example.org   https:\/\/example.org   copyright  "
+},
+{
+  "id": "section-title",
+  "level": "1",
+  "url": "section-title.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Section title",
+  "body": " Section title  Hello  "
+},
+{
+  "id": "colophon-2",
+  "level": "1",
+  "url": "colophon-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
